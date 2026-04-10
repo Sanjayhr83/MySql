804f230f636ff4c8f017e2d281a11fbf4d91c9c4
Sanjay <hrsanjay@gmail.com>
2026-04-10 10:44:05 +0530

    Introduction of MySql

---
+++ 1.sql
@@ -0,0 +1,25 @@
+Datatypes in sql :
+1. characters format datatype (char, varchar, text, binary large object)
+2. numeric formate datatype (integer: int,bigint , decimal)
+3. data formate datatype (data, time, datatime, timestamp)
+
+Contarints:
+1.unique
+2.not null
+3.check
+4.primary key
+5.foreign key
+6.auto_increment
+7.default
+8.enum
+
+keywords:
+1.signed : it will allow both positive and negative values for column
+2.unsigned : it will allow only positive  values for column
+
+types of SQL :
+1.DDL :(Data defination language) Create, alter, truncate, drop
+2.DML :(Data Manuplication language) insert, update, delete
+3.TCL :(Transaction control language) commit, rollback, savepoint
+4.DCL :(Data control language) grant, revoke
+5.DQL :(Data query language) select, selection, projection, join
