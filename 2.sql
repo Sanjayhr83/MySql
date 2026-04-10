3594c9e4c9dfaa6f093115fd513b261a7e47f3cb
Sanjay <hrsanjay@gmail.com>
2026-04-10 11:00:59 +0530

    Some methods use to mysql server

---
+++ 2.sql
@@ -0,0 +1,26 @@
+To create database :
+  Syntax:
+    CREATE DATABASE DATABASE_NAME;
+
+
+To view all the database in Mysql :
+  syntax:
+    SHOW DATABASES;
+
+
+To access particular database in mysql :
+  syntax:
+    USE DATABASE_NAME;
+
+To clear the screen in mysql server :
+  syntax:
+    SYSTEM CLS;
+
+To close mysql server :
+  syntax:
+    EXIT
+
+To view all the tables of one database :
+  syntax:
+    SHOW TABLES;
+
