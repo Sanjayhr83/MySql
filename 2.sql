126b4c0623ae5768902aeee950dc15b82a8f256d
Sanjay <hrsanjay@gmail.com>
2026-04-10 11:24:34 +0530

    Some methods use to mysql server

--- 2.sql
+++ 2.sql
@@ -24,3 +24,8 @@ To view all the tables of one database :
   syntax:
     SHOW TABLES;
 
+To view constaints name of all the contraints in the tables :
+ step 1:-
+        USE INFORMATION_SCHEMA;
+ step 2:-
+        SELECT * FROM TABLE_CONSTRAINTS;
